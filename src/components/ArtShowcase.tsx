db72222474c8b4bc8d4a546ad8c207bd7762a8f5
Prime <primecortex@outlook.com>
2025-03-25 15:42:17 +0600

    ​

--- src/components/ArtShowcase.tsx
+++ src/components/ArtShowcase.tsx
@@ -12,7 +12,7 @@ const ArtShowcase: React.FC<ArtDisplayProps> = ({ art }) => {
   const isEven = Number(id) % 2 === 0;
 
   return (
-    <div className="max-w-5xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4">
       {isEven ? (
         <div className="flex flex-col md:flex-row gap-8">
           <div className="md:w-1/2">
--- src/pages/[artUrl]/index.tsx
+++ src/pages/[artUrl]/index.tsx
@@ -21,7 +21,7 @@ const ArtPage: React.FC = () => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-2">
       <ArtHeader art={art} />
       <ArtShowcase art={art} />
     </div>
