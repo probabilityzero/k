99dc743cd506483c17c4f02d3adf1d84283e193d
Prime <primecortex@outlook.com>
2025-03-27 03:22:35 +0600

    ​

--- src/utils/NotFound.tsx
+++ src/utils/NotFound.tsx
@@ -16,7 +16,6 @@ const NotFound: React.FC = () => {
   // Define an array of image/GIF URLs
   const media = [
     "https://media0.giphy.com/media/WZrOaNjFPKT5e/giphy.gif",
-    "https://www.teahub.io/photos/full/288-2886370_illustration.gif",
     "https://media.giphy.com/media/aGeePr7nv6ra8/giphy.gif",
     "https://i.pinimg.com/originals/cd/9a/cb/cd9acb44872869bd9110cadc8f68b9da.gif",
     "https://68.media.tumblr.com/33e1ac9451cb50a5705ac03aa977d879/tumblr_oobcp0hw9T1v8tshbo1_540.gif",
@@ -27,13 +26,13 @@ const NotFound: React.FC = () => {
   const randomMedia = media[Math.floor(Math.random() * media.length)];
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center-800 p-4">
+    <div className="max-w-7xl mx-auto p-2">
       <header className="mb-4">
         <h2 className="text-xl font-bold p-4 opacity-80">
           I am Nonexistent
         </h2>
       </header>
-      <div className="max-w-xl text-center">
+      <div className="max-w-xl text-center flex flex-col items-center justify-center-800 p-4">
         {randomMedia && (
           <div className="mb-4">
             <img
@@ -43,10 +42,10 @@ const NotFound: React.FC = () => {
             />
           </div>
         )}
-        <h1 className="text-5xl font-extrabold my-2 tracking-tight">
+        <h1 className="text-5xl font-extrabold my-4 tracking-tight">
           Art:// Not Found
         </h1>
-        <p className="text-lg m-4">
+        <p className="text-lg m-2">
           The art piece you seek has eluded the collection. Whether it’s lost in the folds of history or hidden in plain sight, it remains a mystery.
         </p>
         <p className="text-md m-6" style={{ opacity: 0.6 }}>
