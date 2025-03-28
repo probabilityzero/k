2e466cace78e1ea04f30c8ed8bd2391a0aaabf5a
Prime <primecortex@outlook.com>
2025-03-28 12:11:00 +0600

    ​

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-dvh">
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
